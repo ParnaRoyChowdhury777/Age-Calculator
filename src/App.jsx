c74194c642d1c894a997285ab2942c05887316d8
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-02-25 20:42:16 +0530

    Refactor App component and remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useState } from 'react';
-import { AgeCalculatorForm } from './assets/components/AgeCalculatorForm'
-import AgeResult from './assets/components/AgeResult';
+import { AgeCalculatorForm } from './components/AgeCalculatorForm'
+import AgeResult from './components/AgeResult';
 import { differenceInDays, differenceInMonths, differenceInYears } from 'date-fns';
+import Image from './assets/calendar.svg';
 
 const App = () => {
 
@@ -21,12 +22,16 @@ const App = () => {
         });
     };
     return (
-        <div>
-          <h1 className='text-4xl font-bold tracking-wider my-12 text-center'>Age Calculator</h1>
-          <h3 className='text-xl text-center text-stone-400'>Uncover the Secrets of Time: Calculate Your Age with Precision!</h3>
-          <AgeCalculatorForm calculateAge = {calculateAge}/>
-          {age && <AgeResult age={age}/> }
-        </div>
+        <>
+          <div className='absolute left-10 top-20 my-7'>
+            <h1 className='text-5xl font-bold tracking-wider my-12 text-center'>Age Calculator</h1>
+            <h3 className='text-2xl text-center text-stone-400'>Uncover the Secrets of Time: Calculate Your Age with Precision!</h3>
+            <AgeCalculatorForm calculateAge = {calculateAge}/>
+            {age && <AgeResult age={age}/> }
+          </div>
+          <img src={Image} alt='calendar' className='w-1/2 absolute top-0 right-0'/>
+           
+        </>
     )
 };
 
--- src/components/AgeCalculatorForm.jsx
+++ src/components/AgeCalculatorForm.jsx
@@ -4,13 +4,12 @@ import PropTypes from 'prop-types';
 
 export const AgeCalculatorForm = ({calculateAge}) => {
     const [date, setDate] = useState('');
-    const handleChangeDate = (e) => {
-        setDate(e.target.value);
-    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         calculateAge(date);
-    }
+    };
+
     const getCurrentDate = () => {
         const today = new Date();
         const year = today.getFullYear();
@@ -27,12 +26,16 @@ export const AgeCalculatorForm = ({calculateAge}) => {
     
         return `${year}-${month}-${day}`;
       };
+
+    
     
     return (
-    <form onSubmit={handleSubmit}>
-        <input type='date' className='block mx-auto my-10 p-3 border-2 border-gray-300 rounded-md cursor-pointer' value={date} onChange={handleChangeDate} max={getCurrentDate()}/>
-        <button type="submit" className='block mx-auto p-2 bg-green-600 text-white rounded-md hover:bg-green-900' disabled = {!date}>Calculate Age</button>
+    <div>
+    <form onSubmit={handleSubmit} className='flex-col'>
+        <input type='date' className='block mx-auto my-10 p-3 border-2 border-gray-300 rounded-md cursor-text' value={date} onChange={e => setDate(e.target.value)} max={getCurrentDate()}/>
+        <button type="submit" className='block mx-auto p-2 bg-green-600 text-white rounded-md hover:bg-green-900 cursor-pointer' disabled = {!date}>Calculate Age</button>
     </form>
+    </div>
   );
 };
 
