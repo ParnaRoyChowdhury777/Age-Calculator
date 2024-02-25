a85daddc206b81900893f8213a191c94c5c0a776
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2024-02-25 21:16:45 +0530

    Update App title color

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
     return (
       <div className='flex flex-col md:flex-row justify-end'>
       <div className='self-center w-full md:w-1/2'>
-        <h1 className='text-5xl font-bold tracking-wider my-12 text-center'>Age Calculator</h1>
+        <h1 className='text-5xl font-bold tracking-wider my-12 text-center text-slate-600'>Age Calculator</h1>
         <h3 className='text-2xl text-center text-stone-400'>Uncover the Secrets of Time: Calculate Your Age with Precision!</h3>
         <AgeCalculatorForm calculateAge={calculateAge} />
         {age && <AgeResult age={age} />}
